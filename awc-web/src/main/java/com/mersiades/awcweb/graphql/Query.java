9a9fb5ffb9fd5e98453be97509572ce2aafa75d9
Michael Mersiades <fakename014@gmail.com>
2020-11-01 16:44:50 +1100

    Adds allMoves query

--- awc-web/src/main/java/com/mersiades/awcweb/graphql/Query.java
+++ awc-web/src/main/java/com/mersiades/awcweb/graphql/Query.java
@@ -1,21 +1,27 @@
 package com.mersiades.awcweb.graphql;
 
 import com.mersiades.awcdata.models.Game;
+import com.mersiades.awcdata.models.Move;
 import com.mersiades.awcdata.models.User;
 import com.mersiades.awcdata.services.GameService;
+import com.mersiades.awcdata.services.MoveService;
 import com.mersiades.awcdata.services.UserService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class Query implements GraphQLQueryResolver {
 
     private final UserService userService;
     private final GameService gameService;
+    private final MoveService moveService;
 
-    public Query(UserService userService, GameService gameService) {
+    public Query(UserService userService, GameService gameService, MoveService moveService) {
         this.userService = userService;
         this.gameService = gameService;
+        this.moveService = moveService;
     }
 
     public User userByDiscordId(String discordId) {
@@ -26,5 +32,9 @@ public class Query implements GraphQLQueryResolver {
     public Game gameByTextChannelId(String textChannelId) {
         System.out.println("Fetching Game by textChannelId: " + textChannelId);
         return gameService.findGameByTextChannelId(textChannelId);
+    }
+
+    public Set<Move> allMoves() {
+        return moveService.findAll();
     }
 }
