70d7623cacc88af09591db0e25121f07f97268dd
Michael Mersiades <fakename014@gmail.com>
2020-12-17 17:10:16 +1100

    Configures Spring security to allow access to actuator endpoints

--- awc-web/src/main/java/com/mersiades/awcweb/security/WebSecurityConfig.java
+++ awc-web/src/main/java/com/mersiades/awcweb/security/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.mersiades.awcweb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -10,11 +13,16 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -36,5 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/actuator/**"); // for health checks, such as AWS
+        if (isDevOrTest()) {
+            web.ignoring().antMatchers("/graphiql/**");
+        }
+    }
+
+    private boolean isDevOrTest() {
+        List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        return activeProfiles.contains("test") || activeProfiles.contains("dev");
     }
 }
