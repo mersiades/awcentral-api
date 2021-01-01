8813ddd301a07523a9a5fcd03f4a58d80743ed24
Michael Mersiades <fakename014@gmail.com>
2021-01-02 10:59:10 +1100

    Inserts mock data only if mock data doesn't already exist

--- awc-web/src/main/java/com/mersiades/awcweb/bootstrap/MockDataLoader.java
+++ awc-web/src/main/java/com/mersiades/awcweb/bootstrap/MockDataLoader.java
@@ -12,6 +12,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -67,7 +68,7 @@ public class MockDataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
          loadMockData();
 
         System.out.println("Character count: " + Objects.requireNonNull(characterRepository.count().block()).toString());
@@ -79,103 +80,108 @@ public class MockDataLoader implements CommandLineRunner {
     }
 
     private void loadMockData() {
+        List<User> existingUsers = userRepository.findAll().collectList().block();
+        assert existingUsers != null;
+        if (existingUsers.isEmpty()) {
 
-        // -------------------------------------- Set up mock Users -------------------------------------- //
-        User mockUser1 = User.builder()
-                .id(KEYCLOAK_ID_1)
-                .displayName(KEYCLOAK_DISPLAY_NAME_1)
-                .email(KEYCLOAK_EMAIL_1).build();
+            // -------------------------------------- Set up mock Users -------------------------------------- //
+            User mockUser1 = User.builder()
+                    .id(KEYCLOAK_ID_1)
+                    .displayName(KEYCLOAK_DISPLAY_NAME_1)
+                    .email(KEYCLOAK_EMAIL_1).build();
 
-        User mockUser2 = User.builder()
-                .id(KEYCLOAK_ID_2)
-                .displayName(KEYCLOAK_DISPLAY_NAME_2)
-                .email(KEYCLOAK_EMAIL_2).build();
 
-        // ------------------------------ Set up mock Game 1 with Game Roles ----------------------------- //
-        Game mockGame1 = Game.builder()
-                .id(MOCK_GAME_1_ID)
-                .name("Mock Game 1")
-                .commsApp("Zoom")
-                .commsUrl("https://zoom.com/somethingsomething?something=something&somethingelse=somethingelse")
-                .build();
+            User mockUser2 = User.builder()
+                    .id(KEYCLOAK_ID_2)
+                    .displayName(KEYCLOAK_DISPLAY_NAME_2)
+                    .email(KEYCLOAK_EMAIL_2).build();
 
-        GameRole daveAsMC = GameRole.builder().id(DAVE_AS_PLAYER_ID).role(Roles.MC).build();
-        GameRole sarahAsPlayer = GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.PLAYER).build();
+            // ------------------------------ Set up mock Game 1 with Game Roles ----------------------------- //
+            Game mockGame1 = Game.builder()
+                    .id(MOCK_GAME_1_ID)
+                    .name("Mock Game 1")
+                    .commsApp("Zoom")
+                    .commsUrl("https://zoom.com/somethingsomething?something=something&somethingelse=somethingelse")
+                    .build();
 
-        Npc mockNpc1 = Npc.builder().name("Vision").description("Badass truck; driver").build();
-        Npc mockNpc2 = Npc.builder().name("Nbeke").build();
+            GameRole daveAsMC = GameRole.builder().id(DAVE_AS_PLAYER_ID).role(Roles.MC).build();
+            GameRole sarahAsPlayer = GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.PLAYER).build();
 
-        Threat mockThreat1 = Threat.builder().name("Tum Tum").threatKind(Threats.WARLORD).impulse("Slaver: to own and sell people").build();
-        Threat mockThreat2 = Threat.builder().name("Gnarly").threatKind(Threats.GROTESQUE).impulse("Cannibal: craves satiety and plenty").build();
+            Npc mockNpc1 = Npc.builder().name("Vision").description("Badass truck; driver").build();
+            Npc mockNpc2 = Npc.builder().name("Nbeke").build();
 
-        daveAsMC.getNpcs().add(mockNpc1);
-        daveAsMC.getNpcs().add(mockNpc2);
-        daveAsMC.getThreats().add(mockThreat1);
-        daveAsMC.getThreats().add(mockThreat2);
+            Threat mockThreat1 = Threat.builder().name("Tum Tum").threatKind(Threats.WARLORD).impulse("Slaver: to own and sell people").build();
+            Threat mockThreat2 = Threat.builder().name("Gnarly").threatKind(Threats.GROTESQUE).impulse("Cannibal: craves satiety and plenty").build();
 
-        mockGame1.getGameRoles().add(daveAsMC);
-        mockGame1.getGameRoles().add(sarahAsPlayer);
-        mockGame1.setMc(mockUser1);
-        mockGame1.getPlayers().add(mockUser2);
-        gameService.save(mockGame1).block();
+            daveAsMC.getNpcs().add(mockNpc1);
+            daveAsMC.getNpcs().add(mockNpc2);
+            daveAsMC.getThreats().add(mockThreat1);
+            daveAsMC.getThreats().add(mockThreat2);
 
-        mockUser1.getGameRoles().add(daveAsMC);
+            mockGame1.getGameRoles().add(daveAsMC);
+            mockGame1.getGameRoles().add(sarahAsPlayer);
+            mockGame1.setMc(mockUser1);
+            mockGame1.getPlayers().add(mockUser2);
+            gameService.save(mockGame1).block();
+
+            mockUser1.getGameRoles().add(daveAsMC);
 //        userService.save(mockUser1);
 
-        mockUser2.getGameRoles().add(sarahAsPlayer);
+            mockUser2.getGameRoles().add(sarahAsPlayer);
 //        userService.save(mockUser2);
 
 
-        daveAsMC.setUser(mockUser1);
-        daveAsMC.setGame(mockGame1);
-        sarahAsPlayer.setGame(mockGame1);
-        sarahAsPlayer.setUser(mockUser2);
-        gameRoleService.saveAll(Flux.just(daveAsMC, sarahAsPlayer)).blockLast();
+            daveAsMC.setUser(mockUser1);
+            daveAsMC.setGame(mockGame1);
+            sarahAsPlayer.setGame(mockGame1);
+            sarahAsPlayer.setUser(mockUser2);
+            gameRoleService.saveAll(Flux.just(daveAsMC, sarahAsPlayer)).blockLast();
 
-        threatService.saveAll(Flux.just(mockThreat1, mockThreat2)).blockLast();
-        npcService.saveAll(Flux.just(mockNpc1, mockNpc2)).blockLast();
+            threatService.saveAll(Flux.just(mockThreat1, mockThreat2)).blockLast();
+            npcService.saveAll(Flux.just(mockNpc1, mockNpc2)).blockLast();
 
-        // ------------------------------ Set up mock Game 2 with Game Roles ----------------------------- //
-        Game mockGame2 = Game.builder()
-                .id(MOCK_GAME_2_ID)
-                .name("Mock Game 2")
-                .commsApp("Discord")
-                .commsUrl("https://discord.com/something")
-                .build();
+            // ------------------------------ Set up mock Game 2 with Game Roles ----------------------------- //
+            Game mockGame2 = Game.builder()
+                    .id(MOCK_GAME_2_ID)
+                    .name("Mock Game 2")
+                    .commsApp("Discord")
+                    .commsUrl("https://discord.com/something")
+                    .build();
 
-        GameRole daveAsPlayer = GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.PLAYER).build();
-        GameRole sarahAsMC =  GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.MC).build();
+            GameRole daveAsPlayer = GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.PLAYER).build();
+            GameRole sarahAsMC = GameRole.builder().id(UUID.randomUUID().toString()).role(Roles.MC).build();
 
-        Npc mockNpc3 = Npc.builder().name("Batty").description("Overly polite gun for hire").build();
-        Npc mockNpc4 = Npc.builder().name("Farley").build();
+            Npc mockNpc3 = Npc.builder().name("Batty").description("Overly polite gun for hire").build();
+            Npc mockNpc4 = Npc.builder().name("Farley").build();
 
-        Threat mockThreat3 = Threat.builder().name("Fleece").threatKind(Threats.BRUTE).impulse("Hunting pack: to victimize anyone vulnerable").build();
-        Threat mockThreat4 = Threat.builder().name("Wet Rot").threatKind(Threats.AFFLICTION).impulse("Condition: to expose people to danger").build();
+            Threat mockThreat3 = Threat.builder().name("Fleece").threatKind(Threats.BRUTE).impulse("Hunting pack: to victimize anyone vulnerable").build();
+            Threat mockThreat4 = Threat.builder().name("Wet Rot").threatKind(Threats.AFFLICTION).impulse("Condition: to expose people to danger").build();
 
-        sarahAsMC.getNpcs().add(mockNpc3);
-        sarahAsMC.getNpcs().add(mockNpc4);
-        sarahAsMC.getThreats().add(mockThreat3);
-        sarahAsMC.getThreats().add(mockThreat4);
+            sarahAsMC.getNpcs().add(mockNpc3);
+            sarahAsMC.getNpcs().add(mockNpc4);
+            sarahAsMC.getThreats().add(mockThreat3);
+            sarahAsMC.getThreats().add(mockThreat4);
 
-        mockGame2.getGameRoles().add(daveAsPlayer);
-        mockGame2.getGameRoles().add(sarahAsMC);
-        mockGame2.setMc(mockUser2);
-        mockGame2.getPlayers().add(mockUser1);
-        gameService.save(mockGame2).block();
+            mockGame2.getGameRoles().add(daveAsPlayer);
+            mockGame2.getGameRoles().add(sarahAsMC);
+            mockGame2.setMc(mockUser2);
+            mockGame2.getPlayers().add(mockUser1);
+            gameService.save(mockGame2).block();
 
-        mockUser1.getGameRoles().add(daveAsPlayer);
-        mockUser2.getGameRoles().add(sarahAsMC);
-        userService.saveAll(Flux.just(mockUser1, mockUser2)).blockLast();
+            mockUser1.getGameRoles().add(daveAsPlayer);
+            mockUser2.getGameRoles().add(sarahAsMC);
+            userService.saveAll(Flux.just(mockUser1, mockUser2)).blockLast();
 
 
-        daveAsPlayer.setUser(mockUser1);
-        daveAsPlayer.setGame(mockGame2);
-        sarahAsMC.setGame(mockGame2);
-        sarahAsMC.setUser(mockUser2);
-        gameRoleService.saveAll(Flux.just(daveAsPlayer, sarahAsMC)).blockLast();
+            daveAsPlayer.setUser(mockUser1);
+            daveAsPlayer.setGame(mockGame2);
+            sarahAsMC.setGame(mockGame2);
+            sarahAsMC.setUser(mockUser2);
+            gameRoleService.saveAll(Flux.just(daveAsPlayer, sarahAsMC)).blockLast();
 
-        threatService.saveAll(Flux.just(mockThreat3, mockThreat4)).blockLast();
-        npcService.saveAll(Flux.just(mockNpc3, mockNpc4)).blockLast();
+            threatService.saveAll(Flux.just(mockThreat3, mockThreat4)).blockLast();
+            npcService.saveAll(Flux.just(mockNpc3, mockNpc4)).blockLast();
+        }
 
         // ---------------------------------- Add Characters to Players --------------------------------- //
 //        Character mockCharacter1 = new Character("October", sarahAsPlayer, Playbooks.ANGEL, "not much gear");
