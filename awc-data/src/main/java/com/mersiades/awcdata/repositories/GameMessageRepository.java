9bc8f1993754d12afcf8030d19a52b2df475ba6c
Michael Mersiades <fakename014@gmail.com>
2021-03-07 18:43:58 +1100

    Deletes service and repository for GameMessage

--- awc-data/src/main/java/com/mersiades/awcdata/repositories/GameMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mersiades.awcdata.repositories;
-
-import com.mersiades.awcdata.models.GameMessage;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface GameMessageRepository extends ReactiveMongoRepository<GameMessage, String> {
-}
