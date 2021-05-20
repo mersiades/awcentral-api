d9ff62df21850e920917ac99637859ddb3ceb2ef
Michael Mersiades <fakename014@gmail.com>
2021-05-20 14:40:53 +1000

    Improves resetDb method, sets cypress profile to use cloud mongoDB

--- awc-web/src/main/java/com/mersiades/awcweb/services/impl/CypressTestServiceImpl.java
+++ awc-web/src/main/java/com/mersiades/awcweb/services/impl/CypressTestServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,51 +22,39 @@ public class CypressTestServiceImpl implements CypressTestService {
     private final CharacterRepository characterRepository;
     private final GameRepository gameRepository;
     private final GameRoleRepository gameRoleRepository;
-
-    /**
-     * ExecutorService executorService = Executors.newFixedThreadPool(4); // TODO: proper number of threads
-     *
-     * Future<Integer> future1 = executorService.submit(() -> callService1()); // TODO: proper type and method
-     * Future<String> future2 = executorService.submit(() -> callService2()); // TODO: proper type and method
-     *
-     * executorService.shutdown();
-     * executorService.awaitTermination(5, TimeUnit.MINUTES); // TODO: proper timeout
-     *
-     * Integer result1 = future1.get(); // TODO: proper type
-     * String result2 = future2.get(); // TODO: proper type
-     *
-     * Explanation:
-     *
-     * above code creates an ExecutorService with 4 threads, to which you can submit all your tasks (i.e. calls to microservices)
-     * then you call ExecutorService.shutdown (no more task submissions allowed) and ExecutorService.awaitTermination (wait till all tasks are finished)
-     * finally, you call Future.get on all the futures to get the results
-     * note that if a task threw an exception during executing, a call to Future.get will throw an ExecutionException that wraps the exception thrown by the task
-     */
+    private final ThreatRepository threatRepository;
+    private final AllyRepository allyRepository;
+    private final Environment environment;
 
     @Profile("cypress")
     @Override
     public SystemMessage resetDB() {
+        String[] activeProfiles = this.environment.getActiveProfiles();
 
-        try {
-            userRepository.deleteAll();
-            npcRepository.deleteAll();
-            characterRepository.deleteAll();
-            gameRepository.deleteAll();
-            gameRoleRepository.deleteAll();
+        if (activeProfiles.length == 1 && activeProfiles[0].equals("cypress")) {
+            try {
+                // Delete existing mock data
+                allyRepository.deleteAll();
+                characterRepository.deleteAll();
+                gameRepository.deleteAll();
+                gameRoleRepository.deleteAll();
+                npcRepository.deleteAll();
+                threatRepository.deleteAll();
+                userRepository.deleteAll();
 
-            mockDataService.loadMockData();
-
-            long characterCount = characterRepository.count();
-            if (characterCount == 0) {
+                // Reseed with mock data
+                mockDataService.loadMockData();
                 mockDataService.loadMockCharacters();
                 mockDataService.loadHx();
-                mockDataService.loadThreats(); // characterCount is serving as a proxy for threatCount here
-                mockDataService.loadNpcs(); // characterCount is serving as a proxy for npcCount here
-            }
+                mockDataService.loadThreats();
+                mockDataService.loadNpcs();
 
-            return SystemMessage.builder().id(new ObjectId().toString()).successMessage("Database reset").build();
-        } catch (Exception exception){
-            return SystemMessage.builder().id(new ObjectId().toString()).errorMessage("Database reset failed: " + exception.getMessage()).build();
+                return SystemMessage.builder().id(new ObjectId().toString()).successMessage("Database reset").build();
+            } catch (Exception exception) {
+                return SystemMessage.builder().id(new ObjectId().toString()).errorMessage("Database reset failed: " + exception.getMessage()).build();
+            }
+        } else  {
+            return SystemMessage.builder().id(new ObjectId().toString()).errorMessage("Database reset failed: not Cypress server").build();
         }
     }
 }
